e400f7c5730be031e7152923a88d49fb7073f4f9
grimaim <muhammadrahim.mr196@gmail.com>
2024-11-09 01:47:49 +0700

    chore: update app.py and index.py

--- index.py
+++ index.py
@@ -19,19 +19,18 @@ def index():
             "4. Method PUT - Memperbarui Playlist berdasarkan ID": "/api/playlists/<id>",
             "5. Method DELETE - Menghapus Playlist berdasarkan ID": "/api/playlists/<id>",
         },
-        "PlaylistSong APIs": {
-            "1. Method POST - Menambahkan Lagu ke Playlist": "/api/playlistsongs",
-            "2. Method GET - Mengambil semua PlaylistSong": "/api/playlistsongs",
-            "3. Method GET - Mengambil PlaylistSong berdasarkan ID": "/api/playlistsongs/<id>",
-            "4. Method PUT - Memperbarui PlaylistSong berdasarkan ID": "/api/playlistsongs/<id>",
-            "5. Method DELETE - Menghapus PlaylistSong berdasarkan ID": "/api/playlistsongs/<id>",
-        },
         "Song APIs": {
             "1. Method POST - Membuat Song": "/api/songs",
             "2. Method GET - Mengambil semua Song": "/api/songs",
             "3. Method GET - Mengambil Song berdasarkan ID": "/api/songs/<id>",
             "4. Method PUT - Memperbarui Song berdasarkan ID": "/api/songs/<id>",
             "5. Method DELETE - Menghapus Song berdasarkan ID": "/api/songs/<id>",
+        },
+        "PlaylistSong APIs": {
+            "1. Method POST - Menambahkan Lagu ke Playlist": "/api/playlistsongs",
+            "2. Method GET - Mengambil semua PlaylistSong": "/api/playlistsongs",
+            "3. Method PUT - Memperbarui PlaylistSong berdasarkan ID": "/api/playlistsongs/<id>",
+            "4. Method DELETE - Menghapus PlaylistSong berdasarkan ID": "/api/playlistsongs/<id>",
         },
     }
     return jsonify(api_list)
